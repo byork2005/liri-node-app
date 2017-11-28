af653e0f9de8d9c643f6da30b9055921a2a7e88f
Brandon York <30988963+byork2005@users.noreply.github.com>
2017-11-28 16:14:51 -0600

    update 11/28

--- liri.js
+++ liri.js
@@ -1,10 +1,22 @@
 var twitterKeys = require('./keys.js');
 var Twitter = require('twitter');
 var client = new Twitter (twitterKeys);
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 var request = require('request');
 
+var spotify = new Spotify({
+  id: '2659e7f192d0460488f8103f2f001151',
+  secret: 'bc20fcde3edb44978fd3cd9193a6abc7'
+});
+
 var input = process.argv;
+
+console.log("Try these commands! \nmy-tweets \nspotify-this-song '<song name here>' \nmovie-this '<movie name here>' \ndo-what-it-says")
+
+if(input[2] == 'spotify-this-song')
+{
+  musicSearch(input[3]);
+}
 
 function tweets() 
 {
@@ -27,5 +39,20 @@ function tweets()
     });
 }
 
+function musicSearch(song)
+{
+  spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) 
+  {
+    if (err) 
+    {
+      return console.log('Error occurred: ' + err);
+    }
+  
+    // console.log(JSON.stringify(data, null, 2)); 
+    console.log("Name: " + JSON.stringify(data.tracks.items, null, 2));
+  });
+};
 
 
+// ID: 2659e7f192d0460488f8103f2f001151
+// secret: bc20fcde3edb44978fd3cd9193a6abc7
